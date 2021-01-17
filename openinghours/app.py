1a3e7e26e6743b0ae840d55796445fa5b409c78f
ognjenbulut <ognjen.bulut@kiwi.com>
2021-01-17 13:10:19 +0100

    first working version with one validation creating api post, taking json, validating json, returning text value and printing human readable value

--- openinghours/app.py
+++ openinghours/app.py
@@ -9,20 +9,26 @@ api = Api(app)
 class OpeningHours(Resource):
     def post(self):
         result = ""
-        data = request.get_json()
+        data = request.get_json()#list = [time["type"] for time in data.items()]
+
         if data:
             # It wouldn't make much sense to have open after open value, so to have proper json we are creating
             # simple validation that would check if json is properly written, so we have open and close times all the time
-            type_list = [] # creating a list of only types
-            for key in data:
-                times = data[key]
-                for time in times:
-                    type_list.append(time["type"])
-
+            type_list = [time["type"] for key in data for time in data[key]] # creating a list of only types
             validation_list = [type_list[i] == type_list[i-1] for i in range(len(type_list))]  # list comprehension that is checking in case values are repeating one after another and returns True in that case
             if True in validation_list:
                 abort(400, "Please check your json values, looks like you have types repeating one after another")
 
+            # Transforming list so I can do validation for times
+            converted_list = [(key, time["type"], time["value"]) for key in data for time in data[key]]
+            validation_time_list = [(converted_list[i - 1][2] >= converted_list[i][2]) for i in range(len(converted_list)) if
+                         converted_list[i][0] == converted_list[i - 1][0] and converted_list[i][1] == "close" and converted_list[i - 1][
+                             1] == "open"]
+            # Checking if there are wrong time values
+            if True in validation_time_list:
+                abort(400, "Please check your json values, looks like you wrong hours")
+
+            # main method for creating string, could have been done also in comprehension list but looked too complex to be one liner in my opinion
             for key in data:
                 times = data[key]
                 if times:
@@ -36,7 +42,6 @@ class OpeningHours(Resource):
                         # in case there is a day starting with close type time, that means that this closing time belongs to previous day
                         if time == times[0] and times[0]["type"] == "close":
                             result += str(readable_time)  # so we are adding value to previous string, before we add a new day to this string
-                            val_list.append(time["value"])
                         else:
                             if time["type"] == "open":
                                 time_string += str(readable_time) + " - "
@@ -49,7 +54,7 @@ class OpeningHours(Resource):
                 else:
                     time_string = "Closed"
 
-                result += key if key == "monday" else ' \n '+key
+                result += key if key == "monday" else ' \n'+key
                 result += ": "+time_string
 
             print(result)
